469c8b559dfe3da4a5536454e0972a1e801bd67c
tantal <ib.medvedev@rambler.ru>
2013-10-18 12:28:32 +0700

    12:28 18.10.13

--- src/seabattle/Controller.java
+++ src/seabattle/Controller.java
@@ -3,6 +3,7 @@ package seabattle;
 
 public class Controller {
     public static void main(String[] args){
-
+        Field f = new Field();
+        f.showField();
     }
 }
--- src/seabattle/Field.java
+++ src/seabattle/Field.java
@@ -6,16 +6,28 @@ public class Field {
 
     public Field(){
 
-        int hor = 1, size = 10;
+        int hor = 1, size = 10, counter = 0;
         char vert = 'a';
 
         field = new Point[size * size];
         for (Point p : field){
             p = new Point(vert, hor, false);
+            counter++;
             hor++;
             if (hor == 10){
                 hor = 1;
                 vert ++;
+            }
+        }
+
+        System.out.println(counter);
+    }
+
+    public void showField(){
+        for (Point p : field){
+            System.out.print("(" + p.getH() + "," + p.getV() + ") ");
+            if (p.getV() == 10){
+                System.out.println();
             }
         }
     }
